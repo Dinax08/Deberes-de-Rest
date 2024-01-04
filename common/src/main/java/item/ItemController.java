70264555db9acb265b8e733953498cd75d3e350f
Dinax08 <druizgarci07@gmail.com>
2024-01-04 20:26:21 +0100

    Update

--- common/src/main/java/item/ItemController.java
+++ common/src/main/java/item/ItemController.java
@@ -5,8 +5,10 @@ import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import me.dinax.deberes.deberes.DeberesDeRest;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Tiers;
 
 public class ItemController {
     public static final DeferredRegister<Item> ItemRegister = DeferredRegister.create(DeberesDeRest.MOD_ID, Registries.ITEM);
@@ -18,6 +20,10 @@ public class ItemController {
     public static final RegistrySupplier<Item> Stylus = ItemRegister.register(
             "stylus",
             () -> new Item(new Item.Properties().arch$tab(CreativeTabController.DeberesTab))
+    );
+    public static final RegistrySupplier<Item> DualAxe = ItemRegister.register(
+            "dual_axe",
+            () -> new AxeItem(Tiers.NETHERITE, 10, 10, new Item.Properties().arch$tab(CreativeTabController.DeberesTab))
     );
     public static void register(){
         ItemRegister.register();
